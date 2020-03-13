611f2541c836e8f381ae6fb6c52c67044a98b1b4
Gail Fike <gfike19@gmail.com>
2020-03-12 20:58:54 -0500

    started work on studio

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    let takeOffButton = document.getElementById("takeoff");
+    takeOffButton.addEventListener("click", function (event) {
+        let launch = window.confirm("Confirm that the shuttle is ready for takeoff");
+        let flightStatus = document.getElementById("flightStatus");
+        if(launch == true) {
+            flightStatus.innerHTML = "Shuttle in flight";
+           let shuttleBackground = document.querySelector("#shuttleBackground");
+           shuttleBackground.style.background = "blue";
+        }
+        
+    });
+
+
+}
